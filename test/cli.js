5a77083d46a5dc40e85188714a73a4ae2e7609b8
André Ruffert <andre@andreruffert.com>
2016-06-07 14:03:47 +0200

    chore: add basic cli test ✅

---
+++ test/cli.js
@@ -0,0 +1,13 @@
+import test from 'ava';
+import execa from 'execa';
+import pkg from '../package.json';
+
+test('show --version', async t => {
+  t.is(await execa.stdout('../cli.js', ['--version']), pkg.version);
+});
+
+test('show --help', async t => {
+  t.regex(await execa.stdout('../cli.js', ['--help']), new RegExp(pkg.description));
+});
+
+
